d65d1eac2f1894372fc545aea85e56310b8c9578
ColinAlmendro <ColinAlmendro@gmail.com>
2021-09-01 14:28:29 +0200

    finished tutorial 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
-  },
-  plugins: [],
+	siteMetadata: {
+		title: "My First Gatsby Site",
+	},
+	plugins: [
+		"gatsby-plugin-gatsby-cloud",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: `blog`,
+				path: `${__dirname}/blog`,
+			},
+		},
+	],
 };
